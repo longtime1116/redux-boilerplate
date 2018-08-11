6762f8ab2a8a912f44f389955c3da62830f04f92
Tokinaga Shumo <longtime1116@gmail.com>
2018-08-11 23:41:15 +0900

    07. Implementing Store from Scratch

---
+++ src/createStore.js
@@ -0,0 +1,21 @@
+function createStore(reducer) {
+  let state;
+  let listeners = [];
+  function getState() {
+    return state;
+  }
+  function dispatch(action) {
+    state = reducer(state, action);
+    listeners.forEach(listener => listener());
+  }
+  function subscribe(listener) {
+    listeners.push(listener);
+    return () => {
+      listeners = listeners.filter(l => l !== listener);
+    };
+  }
+  dispatch({});
+  return { getState, dispatch, subscribe };
+}
+
+export default createStore;
