f5133a41f69196e5d6e192c906e6e8190d4fe007
Tokinaga Shumo <longtime1116@gmail.com>
2018-08-11 21:44:15 +0900

    05. Writing a Counter Reducer with Tests

--- src/index.js
+++ src/index.js
@@ -1,4 +1,18 @@
-import expect from 'expect';
-import './index.css';
+import expect from "expect";
+import "./index.css";
 
-console.log('all test passed!');
+function counter(state = 0, action) {
+  switch (action.type) {
+    case "INCREMENT":
+      return state + 1;
+    case "DECREMENT":
+      return state - 1;
+    default:
+      return state;
+  }
+}
+expect(counter(1, { type: "INCREMENT" })).toEqual(2);
+expect(counter(2, { type: "DECREMENT" })).toEqual(1);
+expect(counter(1, { type: "UNKNOWN" })).toEqual(1);
+expect(counter(undefined, { type: "UNKNOWN" })).toEqual(0);
+console.log("all test passed!");
