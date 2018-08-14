926fcc352032ac6669fab302bb8ef21fedca9fb7
Tokinaga Shumo <longtime1116@gmail.com>
2018-08-14 21:28:04 +0900

    29. Generating Containers with connect() from React Redux (FooterLink)

--- src/components/FilterLink.js
+++ src/components/FilterLink.js
@@ -1,38 +1,27 @@
+import { connect } from "react-redux";
 import React from "react";
 import Link from "./Link";
-import PropTypes from "prop-types";
 
-export default class FilterLink extends React.Component {
-  componentDidMount() {
-    const { store } = this.context;
-    this.unsubscribe = store.subscribe(() => this.forceUpdate());
-  }
-
-  // Since the subscription happens in `componentDidMount`,
-  // it's important to unsubscribe in `componentWillUnmount`.
-  componentWillUnmount() {
-    this.unsubscribe();
-  }
-  render() {
-    const { store } = this.context;
-    const { filter, children } = this.props;
-    const { visibilityFilter } = store.getState();
-    return (
-      <Link
-        active={visibilityFilter === filter}
-        onClick={() => {
-          store.dispatch({
-            type: "SET_VISIBILITY_FILTER",
-            filter: filter
-          });
-        }}
-      >
-        {children}
-      </Link>
-    );
-  }
+// ownProps として受け取るのではなく {filter} で必要なもののみ受け取っても良い
+// children は無くても動く
+function mapStateToProps({ visibilityFilter }, ownProps) {
+  return {
+    active: visibilityFilter === ownProps.filter,
+    children: ownProps.children
+  };
+}
+function mapDispatchToProps(dispatch, ownProps) {
+  return {
+    onClick: () => {
+      dispatch({
+        type: "SET_VISIBILITY_FILTER",
+        filter: ownProps.filter
+      });
+    }
+  };
 }
 
-FilterLink.contextTypes = {
-  store: PropTypes.object
-};
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Link);
