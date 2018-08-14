3f4846b1cbf2c3792eba94edc81f115f94678b8a
Tokinaga Shumo <longtime1116@gmail.com>
2018-08-14 11:22:29 +0900

    23. Extracting Container Components (VisibleTodoList, AddTodo)(3)

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom";
 import AddTodo from "./components/AddTodo";
 import VisibleTodoList from "./components/VisibleTodoList";
 import Footer from "./components/Footer";
-import store from "./store";
 
 export default function TodoApp() {
   return (
